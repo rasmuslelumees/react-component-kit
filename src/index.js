023aaea4c3d094f5a64e057690ea353938b3830a
Rasmus Lelumees <rasmus.lelumees@gmail.com>
2018-09-24 00:38:59 +0300

    Implement UserCard component with assumptions

--- src/index.js
+++ src/index.js
@@ -3,9 +3,35 @@ import { render } from "react-dom";
 import { UserCard } from "./lib";
 
 const App = () => (
-    <div style={{ width: 800, margin: '20px auto', fontFamily: 'sans-serif', color: '#222' }}>
+    <div style={{ maxWidth: 800, margin: '0 auto', padding: '20px', fontFamily: 'sans-serif', color: '#222' }}>
         <h1>UI Kit</h1>
-        <UserCard content='Test' />
+        <p>Default UserCard component:</p>
+        <div>
+            <UserCard
+                name='Rasmus Lelumees'
+                avatarUrl='https://avatars0.githubusercontent.com/u/7032517?s=460&v=4'
+                statusText='Online | 14h'
+                statusColor='#5ad045'
+            />
+        </div>
+        <p>Minified UserCard component:</p>
+        <div style={{ maxWidth: 150 }}>
+            <UserCard
+                name='Rasmus Lelumees'
+                avatarUrl='https://avatars0.githubusercontent.com/u/7032517?s=460&v=4'
+                statusText='Online | 14h'
+                statusColor='#5ad045'
+            />
+        </div>
+        <p>Applied minimum width UserCard component:</p>
+        <div style={{ maxWidth: 80 }}>
+            <UserCard
+                name='Rasmus Lelumees'
+                avatarUrl='https://avatars0.githubusercontent.com/u/7032517?s=460&v=4'
+                statusText='Online | 14h'
+                statusColor='#5ad045'
+            />
+        </div>
     </div>
 );
 
--- src/lib/UserCard.js
+++ src/lib/UserCard.js
@@ -1,8 +1,85 @@
-import React from "react";
-import "./UserCard.css";
+import React, { Component } from 'react';
+import './UserCard.css';
 
-const UserCard = ({ content = '' }) => (
-    <div className='rui__user-card'>{content}</div>
-);
+const AVATAR_WIDTH_BREAKPOINT = 160;
+const MAX_WIDTH = 180;
+const MIN_WIDTH = 100;
+
+class UserCard extends Component {
+
+    constructor(props){
+        super(props);
+        this.mainRef = React.createRef();
+
+        this.state = {
+            containerWidth: undefined
+        }
+
+        this.updateDimensions = this.updateDimensions.bind(this);
+        this.renderAvatar = this.renderAvatar.bind(this);
+    }
+
+    updateDimensions() {
+        this.setState({ containerWidth: this.mainRef.current.parentNode.clientWidth });
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateDimensions);
+        this.updateDimensions();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
+
+    renderAvatar() {
+        const { avatarUrl, statusColor } = this.props;
+        const { containerWidth } = this.state;
+
+        const avatarStyle = {
+            backgroundImage: `url(${avatarUrl})`
+        };
+
+        const statusStyle = {
+            backgroundColor: `${statusColor}`
+        };
+
+        if (containerWidth < AVATAR_WIDTH_BREAKPOINT) {
+            avatarStyle.backgroundImage = 'none';
+            avatarStyle.backgroundColor = '#fff';
+            avatarStyle.flex = '0 0 12px';
+            statusStyle.position = 'relative';
+            statusStyle.margin = '10px auto';
+        }
+
+        return (
+            <div className='rui__user-card__avatar' style={avatarStyle}>
+                <div className='rui__user-card__avatar-status' style={statusStyle}></div>
+            </div>
+        );
+    }
+
+    render() {
+        const { name, statusText } = this.props;
+        const { containerWidth } = this.state;
+
+        const cardStyle = {
+            width: containerWidth
+        };
+
+        if (containerWidth < MIN_WIDTH) cardStyle.width = MIN_WIDTH;
+        if (containerWidth > MAX_WIDTH) cardStyle.width = MAX_WIDTH;
+
+        return (
+            <div className='rui__user-card' style={cardStyle} ref={this.mainRef}>
+                { this.renderAvatar() }
+                <div className='rui__user-card__info'>
+                    <div className='rui__user-card__name'>{name}</div>
+                    <div className='rui__user-card__status-text'>{statusText}</div>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default UserCard;
