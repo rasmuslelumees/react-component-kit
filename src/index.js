9f3a10eff1789e0d66fdf551c67faee15044e852
Rasmus Lelumees <rasmus.lelumees@gmail.com>
2018-09-23 21:23:19 +0300

    Use correct props

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { UserCard } from "./lib";
 const App = () => (
     <div style={{ width: 800, margin: '20px auto', fontFamily: 'sans-serif', color: '#222' }}>
         <h1>UI Kit</h1>
-        <UserCard label="Email Address" placeholder="name@example.com" />
+        <UserCard content='Test' />
     </div>
 );
 
